442ba070e4a5abfdb3408f149fae4a2b84df1cf5
sajidhossain <sajidhossain171007@gmail.com>
2022-12-27 01:29:42 +0600

    New file

---
+++ Alphabet Rangoli.py
@@ -0,0 +1,18 @@
+import string
+
+alpha = string.ascii_lowercase
+
+
+def print_rangoli(size):
+    list = []
+    for i in range(n):
+        s = "-".join(alpha[i:n])
+        list.append((s[::-1] + s[1:]).center(4 * n - 3, "-"))
+    reversed_list = list[::-1]
+    reversed_list.pop()
+    print('\n'.join(reversed_list + list))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
